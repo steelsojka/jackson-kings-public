49b2b5a4d4029d8e2340edc8409e0ebfbf657154
Steven Sojka <steelsojka@users.noreply.github.com>
2016-04-20 14:45:28 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-// Run when the document is ready for modification.
-document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
-  updateCopyrightYear();
-}, false);
-
-/**
- * Updates the copyright date to the current year.
- */
-function updateCopyrightYear() {
-  document.getElementById('footer-copyright-year').innerText = (new Date()).getFullYear();
-}
+(function() {
+  // Run when the document is ready for modification.
+  document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+    updateCopyrightYear();
+  }, false);
+  
+  /**
+   * Updates the copyright date to the current year.
+   */
+  function updateCopyrightYear() {
+    document.getElementById('footer-copyright-year').innerText = (new Date()).getFullYear();
+  }
+}();
