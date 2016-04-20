9bb060ac7b5759a70d5c9c6edcb032e08e7d4494
Steven Sojka <Steven.Sojka@tdameritrade.com>
2016-04-20 11:53:58 -0500

    add header logo

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Run when the document is ready for modification.
+document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+  updateCopyrightYear();
+}, false);
+
+/**
+ * Updates the copyright date to the current year.
+ */
+function updateCopyrightYear() {
+  document.getElementById('footer-copyright-year').innerText = (new Date()).getFullYear();
+}
