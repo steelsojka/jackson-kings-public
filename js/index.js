43b667a67af89a7cce2002f703787e82c725bfa2
Steven Sojka <steelsojka@gmail.com>
2017-01-14 16:01:58 -0600

    integration

--- js/index.js
+++ js/index.js
@@ -1,35 +1,81 @@
-(function(global) {
-  // Global namespace since we aren't using any build tools.
-  JK.instagram = {
+(function(JK) {
+  JK.InstagramService = {
     // This really shouldn't be stored here, but it's sandboxed and anybody can't really do much with it.
-    accessToken: '202391883.a7b08ff.aa0c792905fd48edaa64f307f0ceef7b'
+    accessToken: '202391883.a7b08ff.aa0c792905fd48edaa64f307f0ceef7b',
+    apiUrl: 'https://api.instagram.com/v1',
+    makeRequest: function(path) {
+      return JK.LoadJSONP(this.apiUrl + path + '?access_token=' + this.accessToken)
+        .then(function(data) {
+          return data.data; 
+        });
+    },
+    getMedia: function() {
+      return this.makeRequest('/users/self/media/recent');
+    }
   };
   
   // Run when the document is ready for modification.
   document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
-  
-  /**
-   * Updates the copyright date to the current year.
-   */
-  function updateCopyrightYear() {
-    document.getElementById('footer-copyright-year').innerText = (new Date()).getFullYear();
-  }
+
+  Vue.component('jk-media-gallery', {
+    template: '#media-gallery-template',
+    data: function() {
+      return {
+        mediaItems: []    
+      };
+    },
+    mounted: function() {
+      return JK.InstagramService.getMedia()
+        .then(function(items) { 
+          this.mediaItems = items; 
+        }.bind(this));
+    }
+  });
 
   // Main function executed on DOM loaded.
   function onDOMContentLoaded() {
-    updateCopyrightYear();
+    new Vue({
+      el: 'footer',
+      data: function() {
+        return {
+          copyrightYear: (new Date()).getFullYear()
+        };
+      }
+    });
+
+    new Vue({
+      el: '#app'
+    });
   }
+}(this.JK));
 
-  /****** Google Analytics ***********/
-  // This is copied straight from google.
-  // Putting it in the main JS file to avoid an extra request.
-  // DO NOT MODIFY.
+// Load data through JSONP... I know...
+(function(JK, global) {
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var scriptNode = document.createElement('script');
+  var uuid = 0;
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  scriptNode.type = 'text/javascript';
+  
+  function LoadJSONP(url) {
+    return new Promise(function(resolve) {
+      var name = '__jsonp' + uuid++;
 
-  ga('create', 'UA-76867928-1', 'auto');
-  ga('send', 'pageview');
-}(this));
+      url += '&callback=' + name;
+
+      var script = scriptNode.cloneNode();
+      script.src = url;
+
+      global[name] = function(data) {
+        resolve(data);
+        head.removeChild(script);
+        script = null;
+        delete global[name];
+      }
+
+      head.appendChild(script);
+    });
+  }
+  
+  JK.LoadJSONP = LoadJSONP;
+})(this.JK, this);
