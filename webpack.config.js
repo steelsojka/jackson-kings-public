8b0606f983274f48d8d80e8bcee64be6e8bb13a2
Steven Sojka <steelsojka@gmail.com>
2017-07-08 07:00:15 -0500

    define production environment for build

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = function(argv) {
     },
     resolve: {
       alias: {
-        'vue': 'vue/dist/vue'
+        'vue': argv.production ? 'vue/dist/vue.min' : 'vue/dist/vue'
       }
     },
     plugins: [
@@ -69,6 +69,11 @@ module.exports = function(argv) {
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: 'production'
         }
       })
     )
