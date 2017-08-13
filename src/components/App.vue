8fa844e6e971d43fa286484b7d070344b3d83ec3
Steven Sojka <steelsojka@gmail.com>
2017-08-13 11:28:02 -0500

    fix(header): change header to reflect page section

--- src/components/App.vue
+++ src/components/App.vue
@@ -15,15 +15,15 @@ export default {
 
 <template>
 <div class="container">
-  <div class="jk-app__title">Music</div>
+  <div id="music" class="jk-app__title">Music</div>
   <div class="content-player">
     <jk-sound-cloud-player></jk-sound-cloud-player>
   </div>
-  <div class="jk-app__title">Events</div>
+  <div id="events" class="jk-app__title">Events</div>
   <div>
     <jk-event-schedule></jk-event-schedule>
   </div>
-  <div class="jk-app__title">Media</div>
+  <div id="media" class="jk-app__title">Media</div>
   <div>
     <jk-media-gallery></jk-media-gallery>
   </div>
